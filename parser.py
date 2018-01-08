7d9f761f4985bf4cd9de69dedce854c41c128eae
Joe Blackman <joseph.blackman@vertivco.com>
2018-01-08 08:26:58 -0500

    Added optional argument for specifying which columns to package in output file Improved strategy for extracting, handling, and packaging data

--- parser.py
+++ parser.py
@@ -9,10 +9,20 @@ def getWorkbook(workbookName):
     workbook = openpyxl.load_workbook(workbookName)
     return workbook
 
+def readTxtFile(txtFile):
+    with open(txtFile, 'r') as f:
+        headerList = f.read()
+        f.close()
+    return headerList
+
 #input: workbook object, name of sheet in workbook object. output: sheet object
 def getSheet(workbook, sheetName):
     sheet = workbook.get_sheet_by_name(sheetName)
     return sheet
+
+def unpack(worksheet):
+    data = [list(x) for x in worksheet] #list comprehension
+    return data
 
 #input: input file path, sheet name, output file path, data to be written. csv created or overwritten to output file path
 def makeCSV(inputFileName, sheetName, outputFileName, table):
@@ -54,56 +64,96 @@ def splitRegisterContent(listOfReg):
 
 def main(argv):
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--iFile", help="Specifies the name of the workbook you wish to import for parsing")
-    parser.add_argument("-s", "--sName", help="Specifies the name of the sheet in the workbook you wish to parse")
-    parser.add_argument("-o", "--oName", help="Specifies the name of the output file you wish to write the parsed data to")
+    parser.add_argument("-i", "--iFile", required=True, help="Specifies the name of the workbook you wish to import for parsing")
+    parser.add_argument("-s", "--sName", required=True, help="Specifies the name of the sheet in the workbook you wish to parse")
+    parser.add_argument("-c", "--columns", help="Specifies the name of the text file with a list of columns the user wants in the output")
+    parser.add_argument("-o", "--oName", required=True, help="Specifies the name of the output file you wish to write the parsed data to")
     args = parser.parse_args()
 
-    inputFileName = args.iFile
+    inputPDSName = args.iFile
     sheetName = args.sName
+    inputHeadersName = args.columns
     outputFileName = args.oName
 
     pds = []
     sheet = []
     try:
-        pds = getWorkbook(inputFileName)
+        pds = getWorkbook(inputPDSName)
     except FileNotFoundError as err:
-        #print(err)
         exit(err)
     except PermissionError as err:
-        #print(err)
         exit(err)
     except TypeError as err:
-        #print(err)
         exit(err)
     try:
         sheet = getSheet(pds, sheetName)
     except KeyError as err:
-        #print(err)
         exit(err)
 
-    sheetAsTuple = tuple(sheet.columns)
+    #extract values from cells, store in list of lists
+    data = []
+    for row in sheet.rows:
+        rowValues = []
+        for cell in row:
+            value = cell.value
+            rowValues.append(value)
+        data.append(rowValues)
 
-    #need to create a list with columns full of values, not cell objects
-    sheetValues = []
-    sheetValuesAsString = []
-    for column in sheetAsTuple:
-        columnValues = []
-        columnValuesAsString = []
-        for cell in column:
-            columnValues.append(cell.value)
-            columnValuesAsString.append(str(cell.value))
-        sheetValues.append(columnValues)
-        sheetValuesAsString.append(columnValuesAsString)
+    originalData = [list(x) for x in data]  # list comprehension
 
-    #prepare to store content extracted from the 'Register Address' column of the pds (register, start bit, bit length)
-    splitRegisterContents = splitRegisterContent(sheetValuesAsString[0])
-    data = zip(splitRegisterContents[0], splitRegisterContents[1], splitRegisterContents[2],
-       sheetValues[3], sheetValues[5], sheetValues[7], sheetValues[8],
-                    sheetValues[9], sheetValues[10], sheetValues[11], sheetValues[13])
+    originalHeaders = []
+    for column in originalData:
+        header = column[0]
+        originalHeaders.append(header)
+
+    # default to keeping all columns if no --columns arg specified
+    desiredHeaders = []
+    if inputHeadersName != None:
+        desiredHeaders = readTxtFile(inputHeadersName)
+    else:
+        desiredHeaders = originalHeaders
+
+    #checks for headers that actually exist
+    matchingHeaders = []
+    for header in desiredHeaders:
+        if header in originalHeaders:
+            matchingHeaders.append(header)
+
+    #if the user specified a register address column, extra parsing is required otherwise, we just return everything
+    #that they ask for
+    finalDataTable = []
+    if 'Register Address' in matchingHeaders:
+        registerColumn = []
+        #search for the column containing register data an copy it
+        for column in originalData:
+            header = column[0]
+            if header == 'Register Address':
+                registerColumn = column
+        #perform split of column
+        splitRegister = splitRegisterContent(registerColumn)
+        #add the 3 columns of the newly split register column to the final data table
+        finalDataTable.append(splitRegister[0])
+        finalDataTable.append(splitRegister[1])
+        finalDataTable.append(splitRegister[2])
+        for column in originalData:
+            header = column[0]
+            for name in matchingHeaders:
+                if (name == header) & (header != 'Register Address'):
+                    finalDataTable.append(column)
+                    #if header != 'Register Address':
+                        #finalDataTable.append(column)
+    else:
+        #no registerColumn specified. don't need to unpack a register column then, just build a list without it
+        for column in originalData:
+            header = column[0]
+            for name in matchingHeaders:
+                if name == header:
+                    finalDataTable.append(column)#will only add columns to the final data table if the headers match
+
+    finalDataTable = zip(*finalDataTable) #formats columns side by side
 
     try:
-        makeCSV(inputFileName, sheetName, outputFileName, data)
+        makeCSV(inputPDSName, sheetName, outputFileName, finalDataTable)
     except FileNotFoundError as err:
         #print(err)
         exit(err)
@@ -115,7 +165,7 @@ def main(argv):
         exit(err)
 
     print("OK")
-    exit()
-#this only executes if modbusMapConversion.py is executed, not imported
+    exit(0)
+
 if __name__ == "__main__":
     main(sys.argv[1:])
